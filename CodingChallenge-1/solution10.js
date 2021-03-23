69736b59d954954885250a532ad6bc8976feecdb
Bodrul Amin <bodrulaminiu@gmail.com>
2021-03-23 23:24:28 +0600

    solutions added

--- CodingChallenge-1/solution10.js
+++ CodingChallenge-1/solution10.js
@@ -1,2 +1,14 @@
-// 10. Write a JavaScript program to check whether a string starts with 'Java' and false otherwise.
+// 10. Write a JavaScript program to check whether a string starts 
+// with 'Java' and false otherwise.
  
+function checkJava(str) {
+
+    var firstFourLetters = str.substring(0,4);
+    if (firstFourLetters=='Java') {
+        return true;
+    }else return false;
+
+ 
+}
+
+console.log(checkJava("JavaSc"))
--- CodingChallenge-1/solution9.js
+++ CodingChallenge-1/solution9.js
@@ -1,2 +1,12 @@
 // 9. Write a JavaScript program to check whether a given positive number is 
 // a multiple of 3 or a multiple of 7.
+
+
+function test3_7(num) {
+    if (num%3==0 || num%7==0) {
+        return true;        
+    }else return false;
+}
+
+
+console.log(test3_7(8));
